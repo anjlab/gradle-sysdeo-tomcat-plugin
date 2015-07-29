2d1ce0c62efde0a1bd8560e351191a69ce468df5
Adam Horner <adam@satago.co.uk>
2015-07-29 18:25:55 +0100

    Add the new nature for the updated uqbar version of the Eclipse plugin

--- src/main/groovy/com/anjlab/gradle/SysdeoEclipseTomcatGenerationExtension.groovy
+++ src/main/groovy/com/anjlab/gradle/SysdeoEclipseTomcatGenerationExtension.groovy
@@ -1,4 +1,4 @@
-/**
+/*
  * Copyright 2013 AnjLab
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
--- src/main/groovy/com/anjlab/gradle/SysdeoEclipseTomcatGeneratorPlugin.groovy
+++ src/main/groovy/com/anjlab/gradle/SysdeoEclipseTomcatGeneratorPlugin.groovy
@@ -1,4 +1,4 @@
-/**
+/*
  * Copyright 2013 AnjLab
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -37,6 +37,11 @@ class SysdeoEclipseTomcatGeneratorPlugin implements Plugin<Project>
                 }
                 // also make it work the successor, uqbar's version
                 nature = 'org.uqbar.eclipse.tomcat.xt.tomcatnature'
+                if (!eclipseProject.natures.find { it.equals( nature ) }) {
+                    eclipseProject.natures.add( nature );
+                }
+                // and the new nature for the uqbar version
+                nature = 'net.sf.eclipse.tomcat.tomcatnature'
                 if (!eclipseProject.natures.find { it.equals( nature ) }) {
                     eclipseProject.natures.add( nature );
                 }
